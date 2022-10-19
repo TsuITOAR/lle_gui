05001084c781ed86f37e50294bfa06e045896455
ITOAR <75484289+TsuITOAR@users.noreply.github.com>
2022-10-19 17:38:42 +0800

    property

---
+++ src/property.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, serde::Deserialize, serde::Serialize)]
+pub(crate) struct Property<T> {
+    pub(crate) value: T,
+    pub(crate) range: (T, T),
+    pub(crate) label: String,
+}
+
+impl Property<f64> {
+    pub fn new(v: f64, label: impl ToString) -> Self {
+        Self {
+            value: v,
+            range: (v - 5., v + 5.),
+            label: label.to_string(),
+        }
+    }
+}
+impl<T> Property<T> {
+    pub fn set(&mut self, v: T) {
+        self.value = v;
+    }
+    pub fn l_range_update(&mut self, l: T) {
+        self.range.0 = l;
+    }
+    pub fn h_range_update(&mut self, h: T) {
+        self.range.1 = h;
+    }
+    pub fn label(&self) -> &str {
+        &self.label
+    }
+}
